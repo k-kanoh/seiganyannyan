1f98e564bb484d4a2f79755f454492e8559a19a8
Kenichi KANOH <k.kanoh@cixi.sakura.ne.jp>
2022-06-17 18:37:24 +0900

    Page forward with the right key

--- 青娥娘娘/青娥娘娘1号/Form1.cs
+++ 青娥娘娘/青娥娘娘1号/Form1.cs
@@ -11,33 +11,24 @@ namespace 青娥娘娘1号
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        private BarcodeWriter writer = new BarcodeWriter()
         {
-            InitializeComponent();
-
-            AllowDrop = true;
-            DragEnter += (_, e) => e.Effect = DragDropEffects.Move;
-            DragDrop += (_, e) => Form1_DragDrop(e);
-        }
-
-        private void Form1_DragDrop(DragEventArgs e)
-        {
-            var drop = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
-
-            var writer = new BarcodeWriter()
+            Format = BarcodeFormat.QR_CODE,
+            Options = new QrCodeEncodingOptions()
             {
-                Format = BarcodeFormat.QR_CODE,
-                Options = new QrCodeEncodingOptions()
-                {
-                    ErrorCorrection = ErrorCorrectionLevel.L,
-                    QrVersion = 40,
-                }
-            };
+                ErrorCorrection = ErrorCorrectionLevel.L,
+                QrVersion = 40,
+            }
+        };
 
+        private byte[] bytes;
+        private int page = 0;
+
+        private void DrawImage()
+        {
             const int maxlen = 2953;
-            const int page = 0;
 
-            var bytes = File.ReadAllBytes(drop);
+            Controls.Clear();
             var encode = Convert.ToBase64String(bytes).Substring(maxlen * 50 * page);
 
             for (int i = 0; i * maxlen < encode.Length && i < 50; i++)
@@ -52,8 +43,34 @@ namespace 青娥娘娘1号
                     Location = new Point((i % 10) * qrImage.Size.Width, 10 + (i / 10) * qrImage.Size.Height)
                 });
             }
+        }
 
+        public Form1()
+        {
+            InitializeComponent();
+
+            AllowDrop = true;
+            DragEnter += (_, e) => e.Effect = DragDropEffects.Move;
+            DragDrop += (_, e) => Form1_DragDrop(e);
+            KeyDown += (_, e) => Form1_KeyDown(e);
+        }
+
+        private void Form1_DragDrop(DragEventArgs e)
+        {
+            var drop = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            bytes = File.ReadAllBytes(drop);
+            DrawImage();
             WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_KeyDown(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                page++;
+                Text = page.ToString();
+                DrawImage();
+            }
         }
     }
 }
